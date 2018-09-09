354c93b7e4b9a10a81799f3c2387d9b06bfd267d
agmullan <33397452+agmullan@users.noreply.github.com>
2018-09-09 14:50:38 -0400

    added BetterRandomAgent

--- search/Agents.py
+++ search/Agents.py
@@ -17,8 +17,9 @@ class DumbAgent(Agent):
             return Directions.STOP
 #"The agent always goes West. "
 #return Directions.WEST
+
 class RandomAgent(Agent):
-    "An agent that choices a random direction and move until you hit wall then choose different direction"
+    "An agent that choices a random direction"
     def getAction(self, state):
         "The agent receives a GameState (defined in pacman.py)."
         print("Location:", state.getPacmanPosition())
@@ -33,3 +34,25 @@ class RandomAgent(Agent):
         else:
             print("list empty")
         return Directions.STOP
+
+class BetterRandomAgent(Agent):
+    "An agent that chooses a random direction with each step but does not choose stop."
+    def getAction(self, state):
+        "The agent receives a GameState (defined in pacman.py)."
+        print("Location:", state.getPacmanPosition())
+        print("Action available:", state.getLegalPacmanActions())
+
+        if len(state.getLegalPacmanActions()) > 0:
+            print("list full")
+            modifiedList = [];
+            for x in state.getLegalPacmanActions():
+                if(x != Directions.STOP):
+                    modifiedList.append(x)
+            print(modifiedList)
+            secure_random = random.SystemRandom()
+            randomDirection = secure_random.choice(modifiedList)
+            print(randomDirection)
+            return randomDirection
+        else:
+            print("list empty")
+            return Directions.STOP
