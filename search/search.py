406a40168839a57e6ff9191437fa82640acad6d8
agmullan <33397452+agmullan@users.noreply.github.com>
2018-10-03 19:52:10 -0400

    added A* search

--- search/search.py
+++ search/search.py
@@ -7,65 +7,54 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 """
-In search.py, you will implement generic search algorithms which are called 
+In search.py, you will implement generic search algorithms which are called
 by Pacman agents (in searchAgents.py).
 """
-
 import util
 
 class SearchProblem:
   """
   This class outlines the structure of a search problem, but doesn't implement
   any of the methods (in object-oriented terminology: an abstract class).
-  
+
   You do not need to change anything in this class, ever.
   """
-  
+
 def getStartState(self):
-   """
-   Returns the start state for the search problem 
-   """
-   util.raiseNotDefined()
+  """
+  Returns the start state for the search problem
+  """
+  util.raiseNotDefined()
 
 def isGoalState(self, state):
-   """
-     state: Search state
+  """
+  state: Search state
 
-   Returns True if and only if the state is a valid goal state
-   """
-   util.raiseNotDefined()
+  Returns True if and only if the state is a valid goal state
+  """
+  util.raiseNotDefined()
 
 def getSuccessors(self, state):
-   """
-     state: Search state
+  """
+  state: Search state
 
-   For a given state, this should return a list of triples, 
-   (successor, action, stepCost), where 'successor' is a 
-   successor to the current state, 'action' is the action
-   required to get there, and 'stepCost' is the incremental 
-   cost of expanding to that successor
-   """
-   util.raiseNotDefined()
+  For a given state, this should return a list of triples,
+  (successor, action, stepCost), where 'successor' is a
+  successor to the current state, 'action' is the action
+  required to get there, and 'stepCost' is the incremental
+  cost of expanding to that successor
+  """
+  util.raiseNotDefined()
 
 def getCostOfActions(self, actions):
-   """
-    actions: A list of actions to take
-
-   This method returns the total cost of a particular sequence of actions.  The sequence must
-   be composed of legal moves
-   """
-   util.raiseNotDefined()
-
-
-def tinyMazeSearch(problem):
   """
-  Returns a sequence of moves that solves tinyMaze.  For any other
-  maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+  actions: A list of actions to take
+
+  This method returns the total cost of a particular sequence of actions.  The sequence must
+  be composed of legal moves
   """
-  from game import Directions
-  s = Directions.SOUTH
-  w = Directions.WEST
-  return  [s,s,w,s,w,w,s,w]
+  util.raiseNotDefined()
+
 
 def tinyMazeSearch(problem):
   """
@@ -78,15 +67,15 @@ def tinyMazeSearch(problem):
   return  [s,s,w,s,w,w,s,w]
 
 def depthFirstSearch(problem):
-    """
+  """
   Search the deepest nodes in the search tree first [p 74].
-  
+
   Your search algorithm needs to return a list of actions that reaches
   the goal.  Make sure to implement a graph search algorithm [Fig. 3.18].
-  
+
   To get started, you might want to try some of these simple commands to
   understand the search problem that is being passed in:
-  
+
   print "Start:", problem.getStartState()
   print "Is the start a goal?", problem.isGoalState(problem.getStartState())
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
@@ -107,16 +96,16 @@ def depthFirstSearch(problem):
   while True:
 
     #no solution
-    if(frontier.isEmpty()): 
-      return [] 
+    if(frontier.isEmpty()):
+      return []
 
     current = frontier.pop() #keep track of the current node
 
     #solution found
-    if (problem.isGoalState(current.getCurrentState())): 
+    if (problem.isGoalState(current.getCurrentState())):
       solution = list()
       step = current
-    
+
       while(step.getParentNode() != None):
           solution.append(step.getLastAction())
           step = step.getParentNode()
@@ -130,7 +119,7 @@ def depthFirstSearch(problem):
 
     #expand the current node
     for successor in problem.getSuccessors(current.getCurrentState()):
-      s = node(successor[0], current, successor[1], successor[2], 1) 
+      s = node(successor[0], current, successor[1], successor[2], 1)
 
       if(s.getCurrentState() in explored):
         print("") #do nothing
@@ -155,35 +144,35 @@ def breadthFirstSearch(problem):
   # path of solution
   path = list()
   while True:
-      #No solution found return empty set
-      if frontier.isEmpty():
-          return []
+    #No solution found return empty set
+    if frontier.isEmpty():
+      return []
 
-      #Dequeue first node in frontier
-      cur_node = frontier.pop()
+    #Dequeue first node in frontier
+    cur_node = frontier.pop()
 
-      #Solution found
-      if problem.isGoalState(cur_node.getCurrentState()):
-          node = cur_node
+    #Solution found
+    if problem.isGoalState(cur_node.getCurrentState()):
+      node = cur_node
 
-          while(node.getParentNode() != None):
-              path.append(node.getLastAction())
-              node = node.getParentNode()
-          # path.append(cur_node.getLastAction())
-          path.reverse()
-          return path
+      while(node.getParentNode() != None):
+        path.append(node.getLastAction())
+        node = node.getParentNode()
 
-      #put cur_node in explored
-      explored[cur_node.getCurrentState()] = "True"
+      path.reverse()
+      return path
 
-      # for every child of cur_node Enqueue to frontier
-      for successor in problem.getSuccessors(cur_node.getCurrentState()):
-          child = node(successor[0], cur_node, successor[1], successor[2], 1)
-          # if the child has not been visited
-          if(child.getCurrentState() not in explored): 
-              # add children to frontier
-              frontier.push(child)
-      
+    #put cur_node in explored
+    explored[cur_node.getCurrentState()] = "True"
+
+    # for every child of cur_node Enqueue to frontier
+    for successor in problem.getSuccessors(cur_node.getCurrentState()):
+      child = node(successor[0], cur_node, successor[1], successor[2], 1)
+      # if the child has not been visited
+      if(child.getCurrentState() not in explored):
+        # add children to frontier
+        frontier.push(child)
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
   "*** YOUR CODE HERE ***"
@@ -197,11 +186,64 @@ def nullHeuristic(state, problem=None):
   return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-  "Search the node that has the lowest combined cost and heuristic first."
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-    
-  
+
+    "Search the node that has the lowest combined cost and heuristic first."
+
+    from util import PriorityQueue
+    from searchAgents import node
+
+    #initialize frontier
+    frontier = PriorityQueue()
+    initialState = problem.getStartState()
+    currentNode = node(initialState, None, 0, 0, 0) #create the root node and add it to the frontier
+
+    frontier.push(currentNode, currentNode.getTotalPathCost())
+
+    #initialize the explored set
+    explored = dict()
+
+    while True:
+        #no solution
+        if(frontier.isEmpty()):
+            return []
+
+        #choose the lowest cost (path + heuristic) node in the frontier
+        current = frontier.pop()
+
+        #solution found
+        if (problem.isGoalState(current.getCurrentState())):
+            solution = list()
+            step = current
+
+            while(step.getParentNode() != None):
+                solution.append(step.getLastAction())
+                step = step.getParentNode()
+
+            solution.reverse()
+
+            return solution #return list of actions from start state to goal state
+
+        #add node to explored
+        explored[current.getCurrentState()] = "True"
+
+        for successor in problem.getSuccessors(current.getCurrentState()):
+            myStepCost = successor[2]
+            myTotalPathCost = current.getTotalPathCost() + myStepCost
+            child = node(successor[0], current, successor[1], myStepCost, myTotalPathCost)
+
+
+
+            if(child.getCurrentState() not in explored):
+                frontier.push(child, child.getTotalPathCost() + heuristic(child.getCurrentState(), problem))
+
+
+
+
+
+
+
+
+
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
